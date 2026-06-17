e7595328d3d7cff86c43785ba2a72bd857cf8aef
Amjad-Hussain-DataScientist <engramjad.hu@gmail.com>
2026-06-17 23:02:18 +0500

    hyperperameter tunning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -41,7 +41,7 @@ class ModelTrainer:
 
     # make another function where we write all code for training the model
 
-    def initiate_model_trainer(self,train_array,test_array):
+    def initiate_model_trainer(self,train_array,test_array,):
         # we will give the output of data transformation.
         try:
             logging.info("splitting the data into train and test")
@@ -63,9 +63,54 @@ class ModelTrainer:
             "AdaBoost Regressor": AdaBoostRegressor(),
             "knn": KNeighborsRegressor()
                 }
+            #hyper perameters tuning 
+            params={
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "knn": {
+                    'n_neighbors':[3,5,7],
+                    'weights': ['uniform', 'distance'],
+                    'algorithm': ['auto', 'ball_tree', 'kd_tree', 'brute'],
+                    },
+                "Gradient Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                
+            }
+
+
             # now make and see which model perform well 
             model_report:dict = evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,
-                                               y_test=y_test,models = models)
+                                               y_test=y_test,models = models,param = params)
                 # evalute_model is commone function  define in utils.py
             # to get best model score from dict 
             best_model_score = max(sorted(model_report.values()))
