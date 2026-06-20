f899ec6b74faa093bdab5df4c874ee9b33d750aa
Amjad-Hussain-DataScientist <engramjad.hu@gmail.com>
2026-06-20 21:12:47 +0500

    degrade python & use sepcific version of dependencies

--- .platform/hooks/prebuild/01_set_swap.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ ! -f /var/swapfile ]; then
-  fallocate -l 2G /var/swapfile
-  chmod 600 /var/swapfile
-  mkswap /var/swapfile
-  swapon /var/swapfile
-  echo "/var/swapfile swap swap defaults 0 0" >> /etc/fstab
-fi
