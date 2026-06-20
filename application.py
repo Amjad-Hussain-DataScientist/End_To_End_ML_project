47e762b56e770bf45312b2825a890ca2545bcf93
Amjad-Hussain-DataScientist <engramjad.hu@gmail.com>
2026-06-21 00:38:17 +0500

    cleanup.config added

--- application.py
+++ application.py
@@ -52,4 +52,4 @@ def predict_datapoint():
     # and we read this result value in html to return the final prediction in frontend 
 # to run the app.py we use 
 if __name__=="__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0",debug = True)
