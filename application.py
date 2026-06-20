f64ebede8e4247a379b8dc98b1c6a965ffb18943
Amjad-Hussain-DataScientist <engramjad.hu@gmail.com>
2026-06-21 02:14:38 +0500

    remove xgboost

--- application.py
+++ application.py
@@ -48,6 +48,4 @@ def predict_datapoint():
         predict_pipline = PredictPipeline()
         # now give the i/p data and predict and store into results
         results = predict_pipline.predict(pred_df)
-        return render_template('home.html', results = results[0]) #because it returning into list format
-    # and we read this result value in html to return the final prediction in frontend 
-# to run the app.py we use 
+        return render_template('home.html', results = results[0]) #because it returning into list format and we read this result value in html to return the final prediction in frontend 
