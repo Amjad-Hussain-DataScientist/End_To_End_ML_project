29b73235a660bf2baa4ec7f99782bbdacf128d26
Amjad-Hussain-DataScientist <engramjad.hu@gmail.com>
2026-06-21 00:46:25 +0500

    remove seaborn/matplotlib for production

--- application.py
+++ application.py
@@ -50,6 +50,3 @@ def predict_datapoint():
         results = predict_pipline.predict(pred_df)
         return render_template('home.html', results = results[0]) #because it returning into list format
     # and we read this result value in html to return the final prediction in frontend 
-# to run the app.py we use 
-if __name__=="__main__":
-    app.run(host="0.0.0.0",debug = True)
