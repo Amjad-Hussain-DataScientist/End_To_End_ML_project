bbc8b965e5577a88bfd08e9d3c4581bf921e3b16
Amjad-Hussain-DataScientist <engramjad.hu@gmail.com>
2026-06-22 01:32:36 +0500

    update dependancies

--- application.py
+++ application.py
@@ -49,3 +49,5 @@ def predict_datapoint():
         # now give the i/p data and predict and store into results
         results = predict_pipline.predict(pred_df)
         return render_template('home.html', results = results[0]) #because it returning into list format and we read this result value in html to return the final prediction in frontend 
+if __name__=="__main__":
+    app.run(host="0.0.0.0", debug =True)
